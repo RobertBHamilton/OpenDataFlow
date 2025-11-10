4a6603bf7aad9b776ed0469beef564eddb351a7c
Robert B Hamilton <robert.bryan.hamilton@gmail.com>
2025-11-10 17:35:12 -0600

    Insert new row for OUT locktype in seed_datastatus
    Added a row indicating that 'someotherjob' produced partition 1.1 of dataset 'bobin' with OUT locktype.

--- examples/seed_datastatus.sql
+++ examples/seed_datastatus.sql
@@ -1,5 +1,6 @@
 -- examples/seed_datastatus.sql
 -- Insert sample rows into dataflow.datastatus demonstrating IN vs OUT locktypes and statuses.
+-- This row indicates that someotherjob produced partition 1.1 of dataset 'bobin' and it is now READY for consumption by any other job.
 INSERT INTO dataflow.datastatus (dataid, datasetid, jobid, locktype, modified, status) VALUES
-  ('1.1','bobin','loadbob','IN','2025-11-05 18:52:26.372454','READY'),
+  ('1.1','bobin','someotherjob','OUT','2025-11-05 18:52:26.372454','READY'),
 ON CONFLICT (dataid, datasetid, jobid) DO NOTHING;
