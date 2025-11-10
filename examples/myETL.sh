7ebb71d721d737b447c7d4b9d9d19498e38ccb3a
Robert B Hamilton <robert.bryan.hamilton@gmail.com>
2025-11-10 16:45:30 -0600

    Add example scripts for dataset-prefixed environment variables and seed data.

---
+++ examples/myETL.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# myETL.sh: Demo script that uses dataset-prefixed environment variables
+
+DATASET_PREFIX="your_dataset_prefix"
+echo "Using dataset prefix: $DATASET_PREFIX"
+# Your ETL logic goes here
